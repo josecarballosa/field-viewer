92cde178a37e3cc0548c845060db4ee7778c8416
Jose Carballosa <jose.carballosa@gmail.com>
2020-01-13 20:37:18 +0100

    =chore: fixed linter configuration

--- ngx-lint.js
+++ ngx-lint.js
@@ -1,26 +1,13 @@
 const { exec } = require('child_process');
-const winston = require('winston');
 
-const logger = winston.createLogger({
-	level: 'debug',
-	format: winston.format.simple(),
-  transports: [new winston.transports.File({ filename: 'ngx-lint.log' })],
-});
-
-const cwd = process.cwd();
 const project = process.argv[2];
 const files = process.argv
   .slice(3)
   .map(f => `--files="${f}"`)
   .join(' ');
 
-logger.debug(`cwd: ${cwd}`);
-logger.debug(`script: ${project}`);
-logger.debug(`files: ${files}`);
-
 exec(`npx ng lint ${project} --fix=true ${files}`, (error, stdout) => {
   if (error) {
-    // logger.error(error);
     console.log(stdout);
     process.exit(1);
   }
